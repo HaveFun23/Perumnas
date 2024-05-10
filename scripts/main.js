0f43c5320427f0199037e191a1980b3db29f28b9
HaveFun23 <ivanivan1879@gmail.com>
2024-05-10 23:02:19 +0300

    немного поменял формат некоторых изображений

--- scripts/main.js
+++ scripts/main.js
@@ -47,3 +47,19 @@ cross.addEventListener("click", function () {
   modal_win.className = "modal_win";
   body.style.overflow = "unset";
 });
+var img_for_adapt = document.querySelector("#img_for_adapt");
+
+setInterval(change, 1000);
+
+function change() {
+  var x = window.innerWidth;
+  if (x <= 1600) {
+    img_for_adapt.src = "./img/background/Hero Section 1600.png";
+  }
+  if (x <= 1300) {
+    img_for_adapt.src = "./img/background/Hero Section 1300.png";
+  }
+  if (x > 1600) {
+    img_for_adapt.src = "./img/background/Hero Section.png";
+  }
+}
